8d16216870f12dca485a0707970871869afa1018
dlvg22 <30533251+dlvg22@users.noreply.github.com>
2017-09-09 13:23:47 +0700

    loop hello

---
+++ helloworld.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>GIT HUB</title>
+</head>
+<body>
+<?php 
+echo 'eduardo';
+	for($a=1;$a<=5;$a++)
+	{
+	$str4="<h$a>hello world</h$a>";
+	echo $str4;
+	}
+?>
+
+</body>
+</html>
